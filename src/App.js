f11e44e8c1ffbe314a22b0c47596fbdf6c1f7e26
Ashwanipydev <pydev.ashwani@gmail.com>
2024-08-21 01:54:19 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AddEmployee from "./components/AddEmployee";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpdateEmployee from "./components/UpdateEmployee ";
 
-const basename = '/em-project-view'
+const basename = '/em-project-view/employees'
 
 function App() {
   return (
